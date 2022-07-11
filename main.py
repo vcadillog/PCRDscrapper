b0a6e717249393883a22a235fa5c000235e645d0
Victor Cadillo <45715531+vcadillog@users.noreply.github.com>
2022-07-10 22:21:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ url = [
         "https://priconne-redive.us/profile/get_profile"
     ]
 
-intents = Intents.default()
-intents.members = True
-bot = Dbot.pvpBot(command_prefix='!', intents=intents)
+bot = Dbot.pvpBot(command_prefix='!')
 bot.add_cog(Dbot.discordPVP(bot,url))
 discordbot = Dbot.discordPVP(bot,url)
 discordbot.start()
@@ -18,4 +16,3 @@ discordbot.start()
 addons = [
     Checker(url, discordbot.ivKey) 
 ]
-
